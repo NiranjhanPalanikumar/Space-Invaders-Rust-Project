59911977ba464205acbb46420e7fae2437ac6476
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-09-22 14:24:51 +0100

    Added a game loop that checks for key_events and exits when either 'Esc' or 'q' is pressed

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,11 +1,26 @@
-use std::error::Error;
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
 use rusty_audio::Audio;
-use std::io;
-use crossterm::{terminal, ExecutableCommand};
-use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
-use crossterm::cursor::{Hide, Show};
+use std::{
+    error::Error,
+    sync::mpsc::{self, Receiver},
+    time::{Duration, Instant},
+    {io, thread},
+};
 
-//-->> Start of main function
+//use std::error::Error;
+//use rusty_audio::Audio;
+//use std::io;
+//use crossterm::{terminal, ExecutableCommand};
+//use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+//use crossterm::cursor::{Hide, Show};
+
+//Main function
+//-------------------------
 fn main() -> Result<(), Box<dyn Error>> {       //making main return a Result so we can use ? ergonomically. <() - for success, Box<dyn Error> box up a dynamic error trait for errors>
     let mut audio = Audio::new();               //creating a mutable audio, imported from rusty_audio
     //adding all audio to our audio manager. syntax -> audio.add("[name]", "[.wav file]");
@@ -27,13 +42,32 @@ fn main() -> Result<(), Box<dyn Error>> {       //making main return a Result so
     stdout.execute(Hide)?;                  //hiding the cursor
 
     
-    
+    //Game loop
+    //---------------------
+    'gameloop: loop{                //named loop so that we can exit from any point necessary
+        
+        //Input handling
+        while event::poll(Duration::default())? {               //poll funcion takes a duration [using default() which is 0]
+            if let Event::Key(key_event) = event::read()? {     //checking for key_events
+                match key_event.code {                          //matching for specific keycodes for different events
+                    KeyCode::Esc | KeyCode::Char('q') => {      //for exiting the game
+                        audio.play("lose");                     //play losing sound because we exited early
+                        break 'gameloop;                        //breaking out of the game loop
+                    }
+                    _ => {}                                     //temporary ignoring all key press to check if game exits when 'Esc' or 'q' is pressed
+                }
+            }
+        };    
+
+    }
+
+
     //Cleanup section
     //-----------------------
 
     //if we leave the audio in the main we wont be able to hear anything coz the audio sys plays the audio in a separate thread in parallel
     //so when the main func ends all threads will be closed
-    //so we add a cleanup section
+
     audio.wait();                               //this will block until all the audio is done playing
     stdout.execute(Show)?;                      //showing the cursor
     stdout.execute(LeaveAlternateScreen)?;      //leaving the alternate screen
