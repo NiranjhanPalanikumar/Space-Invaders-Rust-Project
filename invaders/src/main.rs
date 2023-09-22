031e5e65e3c9b3cc4ae924912b619372b0ce9f21
NiranjhanPalanikumar <niranjhan.p@gmail.com>
2023-09-22 13:20:50 +0100

    Initialsing the teminal, entering alternate screen and hiding cursor. Finally reversing these actions in Cleanup section

--- invaders/src/main.rs
+++ invaders/src/main.rs
@@ -1,5 +1,9 @@
 use std::error::Error;
 use rusty_audio::Audio;
+use std::io;
+use crossterm::{terminal, ExecutableCommand};
+use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
+use crossterm::cursor::{Hide, Show};
 
 //-->> Start of main function
 fn main() -> Result<(), Box<dyn Error>> {       //making main return a Result so we can use ? ergonomically. <() - for success, Box<dyn Error> box up a dynamic error trait for errors>
@@ -15,10 +19,24 @@ fn main() -> Result<(), Box<dyn Error>> {       //making main return a Result so
     //playing the startup sound
     audio.play("startup");
 
+    //Initialising terminal
+    let mut stdout = io::stdout(); //getting acces to stdout
+    terminal::enable_raw_mode()?;  //enable raw mode to get keyboard inputs. The '?' operator is used so thhat the program will crash if we have an error
+
+    stdout.execute(EnterAlternateScreen)?;  //Entering our alternate screen
+    stdout.execute(Hide)?;                  //hiding the cursor
+
+    
+    
+    //Cleanup section
+    //-----------------------
+
     //if we leave the audio in the main we wont be able to hear anything coz the audio sys plays the audio in a separate thread in parallel
     //so when the main func ends all threads will be closed
     //so we add a cleanup section
-    //Cleanup
-    audio.wait();   //this will block until all the audio is done playing
+    audio.wait();                               //this will block until all the audio is done playing
+    stdout.execute(Show)?;                      //showing the cursor
+    stdout.execute(LeaveAlternateScreen)?;      //leaving the alternate screen
+    terminal::disable_raw_mode()?;              //disabling the raw mode
     Ok(())
 }
